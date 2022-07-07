51beba22b750cac15d3e1efcfa5ffd0d4e9120c2
d4rsen <darsenungarlinov00@gmail.com>
2022-07-07 11:34:37 +0300

    deleted fix asc

--- donut.js
+++ donut.js
@@ -2,14 +2,14 @@ let A = 1
 let B = 1
 
 const render = () => {
-    let b = []
-    let z = []
+    const b = []
+    const z = []
     A += 0.07
     B += 0.03
-    let cA = Math.cos(A)
-    let sA = Math.sin(A)
-    let cB = Math.cos(B)
-    let sB = Math.sin(B)
+    const cA = Math.cos(A)
+    const sA = Math.sin(A)
+    const cB = Math.cos(B)
+    const sB = Math.sin(B)
 
     for (let k = 0; k < 1760; k++) {
         b[k] = k % 80 === 79 ? '\n' : ' '
@@ -17,19 +17,19 @@ const render = () => {
     }
 
     for (let j = 0; j < 6.28; j += 0.07) {
-        let ct = Math.cos(j)
-        let st = Math.sin(j)
+        const ct = Math.cos(j)
+        const st = Math.sin(j)
 
         for (let i = 0; i < 6.28; i += 0.02) {
-            let sp = Math.sin(i)
-            let cp = Math.cos(i)
-            let h = ct + 2
-            let D = 1 / (sp * h * sA + st * cA + 5)
-            let t = sp * h * cA - st * sA
-            let x = 0 | (40 + 30 * D * (cp * h * cB - t * sB))
-            let y = 0 | (12 + 15 * D * (cp * h * sB + t * cB))
-            let o = x + 80 * y
-            let N = 0 | (8 * ((st * sA - sp * ct * cA) * cB - sp * ct * sA - st * cA - cp * ct * sB))
+            const sp = Math.sin(i)
+            const cp = Math.cos(i)
+            const h = ct + 2
+            const D = 1 / (sp * h * sA + st * cA + 5)
+            const t = sp * h * cA - st * sA
+            const x = 0 | (40 + 30 * D * (cp * h * cB - t * sB))
+            const y = 0 | (12 + 15 * D * (cp * h * sB + t * cB))
+            const o = x + 80 * y
+            const N = 0 | (8 * ((st * sA - sp * ct * cA) * cB - sp * ct * sA - st * cA - cp * ct * sB))
             if (y < 22 && y >= 0 && x >= 0 && x < 79 && D > z[o]) {
                 z[o] = D
                 b[o] = '.,-~:;=!*#$@'[N > 0 ? N : 0]
